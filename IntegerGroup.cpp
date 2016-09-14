2c40dd2d704cda30edb359a55ec3a2c92a2f5685
MikeWarren2014 <mwarren04011990@gmail.com>
2016-09-14 13:01:15 -0400

    Updated IntegerGroup.cpp to working code

--- IntegerGroup.cpp
+++ IntegerGroup.cpp
@@ -10,6 +10,7 @@ IntegerGroup::IntegerGroup()
 
 IntegerGroup::IntegerGroup(int n)
  : n(n), elements(Array<IntegerGroup::GroupElement>(n))
+   //elements(std::vector<IntegerGroup::GroupElement>(n))
 {
 	//this is to have integers in [0,n-1]
 	for (int j = 0; j < n; j++)
@@ -32,8 +33,8 @@ IntegerGroup::GroupElement::GroupElement()
 	
 }
 
-IntegerGroup::GroupElement::GroupElement(int m) 
-	: m(m),group(0)
+IntegerGroup::GroupElement::GroupElement(int x)
+	: m(x), group(0)
 {
 	
 }
@@ -41,9 +42,16 @@ IntegerGroup::GroupElement::GroupElement(int m)
 IntegerGroup::GroupElement::GroupElement(int m, IntegerGroup * g)
 	: group(g)
 {
-	// this->m must be in [0, g->size() - 1]
-	this->m = m % g->size();
-	if (this->m < 0) this->m = g->size() + this->m;
+	// this->m must be in [0, g->size() - 1], if g not null
+	if (g)
+	{
+		this->m = m % g->size();
+		if (this->m < 0) this->m = g->size() + this->m;
+	}
+	else
+	{
+		this->m = m;
+	}
 }
 
 IntegerGroup::GroupElement::~GroupElement()
@@ -67,14 +75,36 @@ IntegerGroup::GroupElement IntegerGroup::GroupElement::operator*(const IntegerGr
 	// if the group is not defined
 	if (!this->group)
 		// we simply perform integer multiplication
-		return GroupElement(this->val() * b.val());
+		return GroupElement(this->val() * b.val(), this->group);
 	// otherwise, perform group multiplication
-	return GroupElement((this->val() + b.val()) % this->group->size());
+	return GroupElement((this->val() + b.val()) % this->group->size(), this->group);
+}
+
+IntegerGroup::GroupElement IntegerGroup::GroupElement::operator*(int x)
+{
+	if (!this->group)
+		return GroupElement(this->val() * x, this->group);
+	return GroupElement((this->val() * x) % this->group->size());
+}
+
+IntegerGroup::GroupElement operator*(int x, const IntegerGroup::GroupElement& el)
+{
+	return (const_cast <IntegerGroup::GroupElement&> (el) * x);
 }
 
 IntegerGroup::GroupElement IntegerGroup::GroupElement::operator*=(const IntegerGroup::GroupElement& b)
 {
 	return ((*this) = (*this) * b);
+}
+
+IntegerGroup::GroupElement IntegerGroup::GroupElement::operator*=(int x)
+{
+	return ((*this) = (*this) * x);
+}
+
+int IntegerGroup::GroupElement::operator%(int x) const
+{
+	return (this->m % x);
 }
 
 bool IntegerGroup::GroupElement::operator==(const IntegerGroup::GroupElement& b)
@@ -88,5 +118,10 @@ bool IntegerGroup::GroupElement::operator!=(const IntegerGroup::GroupElement& b)
 }
 
 int IntegerGroup::GroupElement::val() const { return this->m; }
+IntegerGroup::GroupElement IntegerGroup::operator[](int x) const 
+{
+	if (x < 0) x += this->n;
+	return this->elements[x % this->n]; 
+}
 
 int IntegerGroup::size() const { return this->n; }
