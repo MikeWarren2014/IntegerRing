c30cc306202a3f743b7eb2d9114ba3bc7539522f
MikeWarren2014 <mwarren04011990@gmail.com>
2016-09-14 13:02:35 -0400

    Updated IntegerRing.cpp

--- IntegerRing.cpp
+++ IntegerRing.cpp
@@ -2,23 +2,57 @@
 #include "IntegerGroup.h"
 #include "Operators.h"
 
-/*IntegerRing::Element::Element()
+#include <iostream>
+
+IntegerRing::Element::Element()
+	: IntegerGroup::GroupElement()
 {
 	
-}*/
-
-/*IntegerRing::Element(const IntegerGroup::GroupElement& el)
-{
-	// copy everything from el into *this
-	this->m = el.m;
-	this->group = el.group;
 }
-/*
+
+IntegerRing::IntegerRing(int n)
+	: IntegerGroup::IntegerGroup(n)
+{
+	
+}
+
+IntegerRing::Element::Element(int m, IntegerGroup * g)
+	: IntegerGroup::GroupElement(m, g)
+{
+	
+}
+
+IntegerRing::Element::Element(const IntegerGroup::GroupElement& el)
+	: IntegerGroup::GroupElement(el)
+{
+	
+}
+
+IntegerRing::Element IntegerRing::Element::operator+(int x)
+{
+	return (IntegerRing::Element)(((IntegerGroup::GroupElement)(*this)) * x);
+}
+
 IntegerRing::Element IntegerRing::Element::operator+(const IntegerRing::Element& b)
 {
 	// IntegerRing is simply Abelian group under addition
 	// thus, we treat the elements like group elements first, multiply under that group, and cast to ring elements
 	return (IntegerRing::Element)(((IntegerGroup::GroupElement)(*this)) * ((IntegerGroup::GroupElement)b));
+}
+
+IntegerRing::Element operator+(int x, const IntegerRing::Element& el)
+{
+	return (const_cast <IntegerRing::Element&>(el) + x);
+}
+
+IntegerRing::Element IntegerRing::Element::operator-()
+{
+	return IntegerRing::Element(-this->m, this->group);
+}
+
+IntegerRing::Element IntegerRing::Element::operator-(int x)
+{
+	return ((*this) - IntegerRing::Element(x, this->group));
 }
 
 IntegerRing::Element IntegerRing::Element::operator-(const IntegerRing::Element& b)
@@ -35,14 +69,31 @@ IntegerRing::Element IntegerRing::Element::operator-(const IntegerRing::Element&
 	// otherwise, val is simply the integer difference of this->m,b.m
 	else val = this->m - b.m;
 	// return element with this value
-	return Element(val);
+	return Element(val, this->group);
+}
+
+
+IntegerRing::Element operator-(int x, const IntegerRing::Element& el)
+{
+	//return (x + -(const_cast<IntegerRing::Element>(el)));
+	return IntegerRing::Element(x - el.m, el.group);
+}
+
+IntegerRing::Element IntegerRing::Element::operator*(int x)
+{
+	return ((*this) * IntegerRing::Element(x, this->group));
 }
 
 IntegerRing::Element IntegerRing::Element::operator*(const IntegerRing::Element& b)
 {
 	if (this->group)
-		return IntegerRing::Element((this->m - b.m) % this->group->size());
-	return IntegerRing::Element(this->m - b.m);
+		return IntegerRing::Element((this->m * b.m) % this->group->size(), this->group);
+	return IntegerRing::Element(this->m * b.m, this->group);
+}
+
+IntegerRing::Element operator*(int x, const IntegerRing::Element& el)
+{
+	return (const_cast<IntegerRing::Element&>(el) * x);
 }
 
 IntegerRing::Element IntegerRing::Element::operator+=(const IntegerRing::Element& b)
@@ -59,13 +110,47 @@ IntegerRing::Element IntegerRing::Element::operator*=(const IntegerRing::Element
 {
 	return ((*this) = (*this) * b);
 }
-*/
+
 IntegerRing::Element IntegerRing::identity(Operators op)
 {
 	// if op is ADDITIVE
 	if (op == ADDITIVE)
 		// return what the base version of this method would return
-		return (IntegerRing::Element)(((IntegerGroup::GroupElement*)this)->identity());
+		return (IntegerRing::Element)(((IntegerGroup)(*this)).identity());
 	// multiplicative identity requested, and it is 1
 	return (IntegerRing::Element)this->elements[0];
+}
+
+IntegerRing::Element IntegerRing::inverse(int x)
+{
+	// make sure x in (0, this->n) . If not, either throw exception or return 0
+	if ((x <= 0) || (x >= this->n)) throw IntegerRing::Element(0, this);
+	// Extended Euclidean Algorithm
+	int t = 0,
+		r = this->n,
+		newT = 1,
+		newR = x;		
+	while (newR != 0)
+	{
+		int quotient = r / newR,
+			tCopy = t,
+			rCopy = r,
+			newTCopy = newT,
+			newRCopy = newR;
+		t = newT;
+		newT = tCopy - quotient * newTCopy;
+		r = newR;
+		newR = rCopy - quotient * newRCopy;
+	}
+	if (r > 1)
+	{
+		throw IntegerRing::Element(0, this);
+	}
+	if (t < 0) t = t + this->n;
+	return IntegerRing::Element(t, this);
+}
+
+IntegerRing::Element IntegerRing::inverse(const IntegerRing::Element& el)
+{
+	return this->inverse(el.val());
 }
