99a642446060cb9270c37692f6f48c9344d7d6bd
MikeWarren2014 <mwarren04011990@gmail.com>
2016-09-14 13:02:10 -0400

    Updated IntegerGroup.h to working code

--- IntegerGroup.h
+++ IntegerGroup.h
@@ -2,6 +2,7 @@
 #define DATAGROUP_H
 
 #include "Array.h"
+#include <vector>	// for some reason, this didn't work.
 
 #include <iostream>
 
@@ -14,15 +15,20 @@ class IntegerGroup
 		IntegerGroup(int);
 		class GroupElement
 		{
-			int m;
-			IntegerGroup* group;
+			protected:
+				int m;
+				IntegerGroup* group;
 			public: 
 				GroupElement();
 				GroupElement(int);
 				GroupElement(int, IntegerGroup*);
 				~GroupElement();
 				GroupElement operator*(const GroupElement&);
+				GroupElement operator*(int);
+				friend GroupElement operator*(int, const GroupElement&);
 				GroupElement operator*=(const GroupElement&);
+				GroupElement operator*=(int);
+				int operator%(int) const;
 				bool operator==(const GroupElement&);
 				bool operator!=(const GroupElement&);
 				int val() const;
@@ -32,16 +38,18 @@ class IntegerGroup
 				}
 				
 		};
+		GroupElement operator[](int) const;
 		GroupElement identity() const;
 		int size() const;
 		friend std::ostream& operator<<(std::ostream& o, const IntegerGroup& g)
 		{
 			return (o << g.elements);
 		}
-	private:
+	protected:
 		int n;
 		//GroupElement * identity;
 		Array<GroupElement> elements;
+		//std::vector<GroupElement> elements;
 		void createNewElement(int);
 };
 
